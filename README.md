c3981762572c1951ffcda21920f554bc5478e02b
angus924 <55837131+angus924@users.noreply.github.com>
2023-08-02 14:05:24 +1000

    Update

--- README.md
+++ README.md
@@ -1,1 +1,68 @@
-# quant
+# QUANT
+
+***QUANT: A Minimalist Interval Method for Time Series Classification***
+
+[arXiv:](https://arxiv.org/abs/) (preprint) [TBA]
+
+
+> <div align="justify">We show that it is possible to achieve the same accuracy, on average, as the most accurate existing interval methods for time series classification on a standard set of benchmark datasets using a single type of feature (quantiles), fixed intervals, and an 'off the shelf' classifier. This distillation of interval-based approaches represents a fast and accurate method for time series classification, achieving state-of-the-art accuracy on the expanded set of 142 datasets in the UCR archive with a total compute time (training and inference) of less than 15 minutes using a single CPU core.</div>
+
+Please cite as:
+
+```bibtex
+@article{dempster_etal_2023,
+  author  = {Dempster, Angus and Schmidt, Daniel F and Webb, Geoffrey I},
+  title   = {{QUANT}: A Minimalist Interval Method for Time Series Classification},
+  year    = {2023},
+  journal = {arXiv:}
+}
+```
+
+## Results
+
+#### UCR Archive (142 Datasets, 30 Resamples)
+
+* [Mean Accuracy + Training/Test Times](./results/accuracy_mean_ucr142.csv)
+* [Per Resample](./results/accuracy_resamples_ucr142.csv)
+
+## Requirements
+
+* Python
+* PyTorch
+* NumPy
+* scikit-learn (or similar)
+
+## Code
+
+### [`quant.py`](./code/quant.py)
+
+## Examples
+
+```python
+from quant import Quant
+from sklearn.ensemble import ExtraTreesClassifier
+
+[...] # load data
+
+transform = Quant()
+
+X_training_transform = transform.fit_transform(X_training, Y_training)
+X_test_transform = transform.transform(X_test)
+
+classifier = \
+ExtraTreesClassifier(
+    n_estimators = 200,
+    max_features = 0.1,
+    criterion = "entropy",
+    n_jobs = -1
+)
+classifier.fit(X_training_transform, Y_training)
+
+predictions = classifier.predict(X_test_transform)
+```
+
+## Acknowledgements
+
+We thank Professor Eamonn Keogh and all the people who have contributed to the UCR time series classification archive.
+
+<div align="center">:zap:</div>
---
+++ code/quant.py
@@ -0,0 +1,163 @@
+# Angus Dempster, Daniel F Schmidt, Geoffrey I Webb
+
+# QUANT: A Minimalist Interval Method for Time Series Classification
+# https://arxiv.org/abs/
+
+import numpy as np
+
+import torch, torch.nn.functional as F
+
+# == generate intervals ========================================================
+
+def make_intervals(input_length, depth):
+
+    exponent = \
+    min(
+        depth,
+        int(np.log2(input_length)) + 1
+    )
+
+    intervals = []
+
+    for n in 2 ** torch.arange(exponent):
+
+        indices = torch.linspace(0, input_length, n + 1).long()
+
+        intervals_n = torch.stack((indices[:-1], indices[1:]), 1)
+
+        intervals.append(intervals_n)
+
+        if n > 1 and intervals_n.diff().median() > 1:
+
+            shift = int(np.ceil(input_length / n / 2))
+
+            intervals.append((intervals_n[:-1] + shift))
+
+    return torch.cat(intervals)
+
+# == quantile function =========================================================
+
+def f_quantile(X, div = 4):
+
+    n = X.shape[-1]
+
+    if n == 1:
+
+        return X
+    
+    else:
+        
+        num_quantiles = 1 + (n - 1) // div
+
+        if num_quantiles == 1:
+
+            return X.quantile(torch.tensor([0.5]), dim = -1).permute(1, 2, 0)
+        
+        else:
+            
+            quantiles = X.quantile(torch.linspace(0, 1, num_quantiles), dim = -1).permute(1, 2, 0)
+            quantiles[..., 1::2] = quantiles[..., 1::2] - X.mean(-1, keepdims = True)
+
+            return quantiles
+
+# == interval model (per representation) =======================================
+
+class IntervalModel():
+
+    def __init__(self, input_length, depth = 6, div = 4):
+
+        assert div >= 1
+        assert depth >= 1
+
+        self.div = div
+
+        self.intervals = \
+        make_intervals(
+            input_length = input_length,
+            depth        = depth,
+        )
+
+    def fit(self, X, Y):
+
+        pass
+
+    def transform(self, X):
+
+        features = []
+
+        for a, b in self.intervals:
+
+            features.append(
+                f_quantile(X[..., a:b], div = self.div).squeeze(1)
+            )
+        
+        return torch.cat(features, -1)
+
+    def fit_transform(self, X, Y):
+
+        self.fit(X, Y)
+        
+        return self.transform(X)
+    
+# == quant =====================================================================
+
+class Quant():
+
+    def __init__(self, depth = 6, div = 4):
+
+        assert depth >= 1
+        assert div >= 1
+
+        self.depth = depth
+        self.div = div
+
+        self.representation_functions = \
+        (
+            lambda X : X,
+            lambda X : F.avg_pool1d(F.pad(X.diff(), (2, 2), "replicate"), 5, 1),
+            lambda X : X.diff(n = 2),
+            lambda X : torch.fft.rfft(X).abs(),
+        )
+
+        self.models = {}
+
+        self.fitted = False
+
+    def transform(self, X):
+
+        assert self.fitted, "not fitted"
+
+        features = []
+
+        for index, function in enumerate(self.representation_functions):
+
+            Z = function(X)
+
+            features.append(
+                self.models[index].transform(Z)
+            )
+        
+        return torch.cat(features, -1)
+    
+    def fit_transform(self, X, Y):
+
+        features = []
+
+        for index, function in enumerate(self.representation_functions):
+
+            Z = function(X)
+
+            self.models[index] = \
+            IntervalModel(
+                input_length = Z.shape[-1],
+                depth        = self.depth,
+                div          = self.div
+            )
+
+            features.append(
+                self.models[index].fit_transform(Z, Y)
+            )
+        
+        self.fitted = True
+        
+        return torch.cat(features, -1)
